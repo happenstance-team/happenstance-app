d17457634cdda5235f74032e715c9db7820a5d41
yoann lopez <yo@yoann.co>
2016-06-08 16:00:05 +0200

    adding react gems and routes

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,3 +1,8 @@
 class Message < ActiveRecord::Base
+  belongs_to :user
   belongs_to :conversation
+
+  validates :content, presence: true
+  validates :conversation, presence: true
+  validates :user, inclusion: { in: :users }
 end
