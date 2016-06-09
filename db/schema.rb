bb07789f96add7b81ac6977d2455f4e80cb484da
yoann lopez <yo@yoann.co>
2016-06-09 10:26:48 +0200

    pushing to heroku

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160608173223) do
+ActiveRecord::Schema.define(version: 20160608192507) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -74,6 +74,7 @@ ActiveRecord::Schema.define(version: 20160608173223) do
     t.string   "first_name"
     t.string   "last_name"
     t.integer  "age"
+    t.string   "avatar_url"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,24 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+edward = User.create!(email: "edward@lewagon.com", password: "azertyuiop", first_name: "Edward", avatar_url: "http://graph.facebook.com/10152948868146275/picture")
+boris = User.create!(email: "boris@lewagon.com", password: "azertyuiop", first_name: "Boris", avatar_url: "https://avatars.githubusercontent.com/u/2471555?v=3")
+seb = User.create!(email: "seb@lewagon.com", password: "azertyuiop", first_name: "Seb", avatar_url: "https://avatars.githubusercontent.com/u/414418?v=3")
+romain = User.create!(email: "romain@lewagon.com", password: "azertyuiop", first_name: "Romain", avatar_url: "https://avatars.githubusercontent.com/u/6377103?v=3")
+
+c1 = Conversation.create!(user1: edward, user2: boris)
+c2 = Conversation.create!(user1: edward, user2: seb)
+c3 = Conversation.create!(user1: edward, user2: romain)
+c4 = Conversation.create!(user1: boris, user2: seb)
+c5 = Conversation.create!(user1: boris, user2: romain)
+c6 = Conversation.create!(user1: seb, user2: romain)
+
+m1 = Message.create!(user: edward, conversation: c1, content: "Hey")
+m2 = Message.create!(user: edward, conversation: c2, content: "Hello")
+m3 = Message.create!(user: edward, conversation: c3, content: "What's up?")
+m4 = Message.create!(user: boris, conversation: c1, content: "Salut Edward")
+m5 = Message.create!(user: seb, conversation: c2, content: "Yo!")
+m6 = Message.create!(user: romain, conversation: c3, content: "Tu peux pas test")
+m7 = Message.create!(user: boris, conversation: c4, content: "Salut Seb")
+m8 = Message.create!(user: boris, conversation: c5, content: "Salut Romain")
+m9 = Message.create!(user: seb, conversation: c6, content: "Hey!")
+m10 = Message.create!(user: seb, conversation: c4, content: "Hello")
+m11 = Message.create!(user: romain, conversation: c5, content: "Quoi de neuf ?")
+m12 = Message.create!(user: romain, conversation: c6, content: "Tu fais quoi demain ?")
